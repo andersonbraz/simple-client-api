75899c31d897f59b5ca242b2326c25052823e69a
Anderson Braz <contato@andersonbraz.com>
2025-05-22 19:49:30 -0300

    Update Files(s).

--- step_03_zone_analytics.py
+++ step_03_zone_analytics.py
@@ -2,13 +2,16 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql import types as T
 
+import pandas as pd
+import sqlite3
+
 spark = SparkSession.builder \
     .appName("ZoneAnalytics") \
     .master("local[*]") \
     .getOrCreate()
 
 df_curated = spark.read \
-    .parquet("data/curated/microsoft_repos")
+    .parquet("data/curated/repos.parquet")
 
 df_analytics = df_curated.select(
     "*"
@@ -17,6 +20,11 @@ df_analytics = df_curated.select(
 df_analytics.show()
 df_analytics.printSchema()
 
-df_analytics.write.mode("overwrite").parquet("data/analytics/microsoft_repos")
+df_analytics.write.mode("overwrite").parquet("data/analytics/repos")
+
+
+df_final = pd.read_parquet("data/analytics/repos")
+conn = sqlite3.connect("github.db")
+df_final.to_sql("repos", conn, if_exists="replace", index=False)
 
 spark.stop()
