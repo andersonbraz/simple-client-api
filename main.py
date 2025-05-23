c45e32fffabf14c7fea7c5d1fed928cbb54656ac
Anderson Braz <contato@andersonbraz.com>
2025-05-23 15:59:24 -0300

    Update Project.

--- main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import sqlite3
-
-df = pd.read_parquet("data/analytics/repos.parquet")
-conn = sqlite3.connect("github.db")
-df.to_sql("repos", conn, if_exists="replace", index=False)
